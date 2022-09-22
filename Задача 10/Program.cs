226a0310ab659b9a945d44bbeacba21c4ef4aa7f
Konstantin <kzhestovskih2012@gmail.com>
2022-09-22 09:00:56 +0500

    Задача 13

--- Задача 10/Program.cs
+++ Задача 10/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
 Console.WriteLine("Введите трехзначное число: ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine(a / 10 % 10);
---
+++ Задача 13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+if (num < 100){
+    Console.WriteLine("Третьей цифры нет");
+    return;
+} 
+if (num > 100 & num < 1000){
+    Console.WriteLine($"Третья цифра - {num % 10}");
+}
+else {
+    while (num > 999){
+        num = num /10;
+    }
+    Console.WriteLine($"Третья цифра - {num % 10}");
+}
